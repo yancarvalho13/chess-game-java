b1ed7a57c2239543275a83f991f29ce92857c794
Yan Carvalho <yansilva303@gmail.com>
2024-01-02 21:38:23 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import boardGame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Position pos = new Position(4, 2);
+		System.out.println(pos);
 
 	}
 
